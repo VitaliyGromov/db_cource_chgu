994d483acac66db935ce8c92971744712842c6d2
Vitaliy Gromov <101171374+VitaliyGromov@users.noreply.github.com>
2026-03-27 18:56:42 +0300

    Update Laba2.sql

--- Laba2.sql
+++ Laba2.sql
@@ -114,3 +114,180 @@ $$;
 CALL Otobr_obuch_po_FIO_Gromov('I'); 
 
 SELECT * from vyv2;
+
+create view V1 as
+SELECT Ocenki.Ocenka1 AS Expr1, Obuchaushiesa.FIO, Obuchaushiesa.Pol, Obuchaushiesa.Data_r, Obuchaushiesa.rodit, Obuchaushiesa.Adres, Obuchaushiesa.tel, 
+Obuchaushiesa.pasp, Obuchaushiesa.no_stb, Obuchaushiesa.data_p, Obuchaushiesa.gruppa, Obuchaushiesa.kurs, Obuchaushiesa.kod_napr, 
+Obuchaushiesa.och_fo, Ocenki.data_ekz1, Ocenki.kod_disc1, Ocenki.data_ekz2, Ocenki.kod_disc2, Ocenki.Ocenka2 AS Expr2, 
+Ocenki.data_ekz3, Ocenki.kod_disc3, Ocenki.Ocenka3 AS Expr3, Ocenki.srednij_ball
+FROM Ocenki INNER JOIN Obuchaushiesa ON Ocenki.kod_obuch = Obuchaushiesa.kod_ob;
+                         
+CREATE or replace PROCEDURE Otobr_ob_SR_b_Gromov
+(Grade Real=0)
+language sql
+AS $$
+DROP TABLE IF EXISTS Vyv2;
+SELECT * INTO Vyv2 FROM V1 WHERE (Expr1+Expr2+Expr3)/3>Grade;
+$$;
+
+CALL Otobr_ob_SR_b_Gromov();
+
+SELECT * from vyv2;
+
+CREATE OR REPLACE FUNCTION F_sred_3_vel_Gromov
+ (Value1 Int, Value2 Int, Value3 Int)
+RETURNS Real
+AS $func$
+SELECT (Value1+Value2+Value3+0.0)/3.0;
+$func$ LANGUAGE SQL;
+
+SELECT F_sred_3_vel_Gromov(3, 5, 5);
+
+CREATE OR REPLACE FUNCTION Posl_den_mes_Gromov
+ (MyDate Date)
+RETURNS Date 
+AS $$ 
+	DECLARE Year Int;
+	 Month Int;
+	 Day Int;
+     TmpDate VarChar(10) ;
+     Result Date;
+BEGIN 
+  Year:= date_part('year',  MyDate);
+  Month:= date_part('month',  MyDate);
+  Day:= date_part('day',  MyDate) ;
+IF Month=12 THEN
+Month:=1;
+Year=Year+1;
+ELSE
+Month:=Month+1;
+END IF;
+TmpDate:=CONCAT(CAST(Month AS varchar(2)),'/01/',CAST(Year AS varchar(4)));
+Result:=CAST(TmpDate AS Date);
+Result:= Result - '1 day'::interval;
+RETURN Result;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT Posl_den_mes_Gromov('07/12/08');
+
+CREATE OR REPLACE FUNCTION F_otb_po_vosr_Gromov()
+RETURNS TABLE (fio2 VARCHAR(50), DR Date, Vozr INT) 
+AS $$
+BEGIN
+RETURN QUERY SELECT FIO, Data_r , CAST(date_part('year',  Current_date)- date_part('year', Data_r) AS INT)
+FROM Obuchaushiesa;
+end;
+$$ LANGUAGE 'plpgsql';
+
+SELECT * FROM F_otb_po_vosr_Gromov();
+
+CREATE OR REPLACE FUNCTION aft_insert_Gromov_fnc()
+  RETURNS trigger AS
+$$
+BEGIN
+ INSERT INTO vyvod 
+VALUES(0, CONCAT('Запись добавлена ', CAST(current_timestamp AS CHAR(25))),0);
+RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER employee_insert_trigger
+  AFTER INSERT
+  ON Obuchaushiesa
+  EXECUTE PROCEDURE aft_insert_Gromov_fnc();
+  
+INSERT INTO Obuchaushiesa
+VALUES
+(13,'F. I. O.', 'Neizv', '1983-12-12', 'Otec, Mat', 'Moskwa', '+74957895674', '8567-567543', 13245, '2007-01-09', 'MM11', 1, 1, 1);
+
+SELECT * FROM vyvod;
+
+CREATE OR REPLACE FUNCTION aft_update_Gromov_fnc()
+  RETURNS trigger AS
+$$
+BEGIN
+ INSERT INTO vyvod 
+VALUES(0, CONCAT('Запись изменена ', CAST(current_timestamp AS CHAR(25))),0);
+RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER Gromov_upd_trigger
+  AFTER UPDATE
+  ON Obuchaushiesa
+  EXECUTE PROCEDURE aft_update_Gromov_fnc();
+
+UPDATE Obuchaushiesa SET adres='Samara' WHERE fio='Ivanov A.I.';
+SELECT * FROM vyvod;
+
+CREATE OR REPLACE FUNCTION aft_delete_Gromov_fnc()
+  RETURNS trigger AS
+$$
+BEGIN
+ INSERT INTO vyvod 
+VALUES(0, CONCAT('Запись удалена ', CAST(current_timestamp AS CHAR(25))),0);
+RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER Familia_del_trigger
+  AFTER DELETE
+  ON Obuchaushiesa
+  EXECUTE PROCEDURE aft_delete_Gromov_fnc();
+
+DELETE FROM Obuchaushiesa WHERE fio='Ivanov A.I.';
+
+SELECT * FROM vyvod;
+
+CREATE TABLE "Employee"
+(
+"EmployeeId" INT NOT NULL,
+"LastName" VARCHAR(20) NOT NULL,
+"FirstName" VARCHAR(20) NOT NULL,
+"Title" VARCHAR(30),
+"ReportsTo" INT,
+"BirthDate" TIMESTAMP,
+"HireDate" TIMESTAMP,
+"Address" VARCHAR(70),
+"City" VARCHAR(40),
+"State" VARCHAR(40),
+"Country" VARCHAR(40),
+"PostalCode" VARCHAR(10),
+"Phone" VARCHAR(24),
+"Fax" VARCHAR(24),
+"Email" VARCHAR(60),
+CONSTRAINT "PK_Employee" PRIMARY KEY  ("EmployeeId")
+);
+
+CREATE TABLE "Employee_Audit"
+("EmployeeId" INT NOT NULL,
+"LastName" VARCHAR(50) NOT NULL,
+"FirstName" VARCHAR(50) NOT NULL,
+"UserName" VARCHAR(50) NOT NULL,
+"EmpAdditionTime" VARCHAR(50) NOT NULL);
+
+CREATE OR REPLACE FUNCTION Gromov_insert_trigger_fnc()
+  RETURNS trigger AS
+$$
+BEGIN
+INSERT INTO "Employee_Audit" ( "EmployeeId", "LastName", "FirstName","UserName" ,"EmpAdditionTime")
+VALUES(NEW."EmployeeId",NEW."LastName",NEW."FirstName",current_user,current_date);
+RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER Gromov_insert_trigger
+  AFTER INSERT
+  ON "Employee"
+  FOR EACH ROW
+  EXECUTE PROCEDURE Gromov_insert_trigger_fnc();
+
+INSERT INTO "Employee" 
+VALUES(12,' Smith','Jeff','Editor',1,'1992-05-28 00:00:00','2022-01-15 00:00:00','Paseo de Gracia','Barcelona','Catalonia','Spain','128 665','+15 52-469-2573','+15 52-469-2573','mail@mail.com');
+
+SELECT * FROM "Employee_Audit";
